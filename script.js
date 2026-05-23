175790e63202ba9aabca70638907b29fd9f9186e
Mahalakshmi <maha.sahasraa@gmail.com>
2026-05-23 19:57:17 +0530

    added javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+function calculate() {
+    let studentName = document.getElementById("name").value;
+    let tamil = parseInt(document.getElementById("tamil").value);
+    let english = parseInt(document.getElementById("english").value);
+    let maths = parseInt(document.getElementById("maths").value);
+    let science = parseInt(document.getElementById("science").value);
+    let social = parseInt(document.getElementById("social").value);
+    let total = tamil + english + maths + science + social;
+    let percentage = total / 5;
+    let grade;
+    
+    if (percentage >= 90) {
+        grade = "A";
+    } else if (percentage >= 75) {
+        grade = "B";
+    } else if (percentage >= 50) {
+        grade = "C";
+    } else {
+        grade = "Fail";
+    }
+    document.getElementById("result").innerHTML = ` <h2>Student Result</h2>
+    Student Name : ${studentName}
+    <br><br>
+    Total Marks : ${total}
+    <br><br>
+    Percentage : ${percentage}%
+    <br><br>
+    Grade : ${grade}`;
+
+    document.getElementById("form").style.display = "none";
+    document.getElementById("resultSection").style.display = "block";
+}
+
+
+function goBack(){
+    document.getElementById("resultSection") .style.display = "none";
+    document.getElementById("form").style.display = "block";
+
+}
